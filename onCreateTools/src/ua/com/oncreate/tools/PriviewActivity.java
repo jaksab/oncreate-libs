2d89438e80547524519f8b49dd471f3e3527e00a
jaksab <jaksab2@mail.ru>
2014-07-12 17:08:38 +0300

    first version

---
+++ onCreateTools/src/ua/com/oncreate/tools/PriviewActivity.java
@@ -0,0 +1,17 @@
+package ua.com.oncreate.tools;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class PriviewActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_priview);
+	}
+
+
+
+}
